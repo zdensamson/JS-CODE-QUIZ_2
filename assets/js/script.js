b15fda84c0a4ce24b2c3963aa9971cee2ef9aaa9
Deku <zachdsamson@gmail.com>
2022-12-15 17:28:27 -0700

    eloquent JS prac problems

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,53 @@
 var number = 5;
 
-var twoFactorial= function(number) {
-    newNum = 2;
-    for (var i = 0; i < number -1; i++){
-        newNum = newNum * 2;
+var triangle= function() {
+    let hash = ''
+    for (i=0; i<7; i++) {
+        hash = hash + '#'
+        console.log(hash);
     }
-    console.log(newNum);
 };
 
+var fizzBuzz = function() {
+    for (i=1; i<=100 ; i++) {
+        if (i%3 == 0 && i%5 ==0){
+            console.log('fizzbuzz')
+        } else if(i%3==0){
+            console.log('fizz');
+        } else if (i%5 == 0) {
+            console.log('buzz');
+        } else {
+            console.log(i);
+        }
 
-twoFactorial(number);
+    }
+}
+
+let size = 10;
+var chessBoard = function(size) {
+    let rowOne = ' ';
+    let rowTwo = '#';
+
+    for(i=0; i<size; i++){
+        if(i%2 == 0) {
+            rowOne = rowOne + '#';
+            rowTwo = rowTwo + ' ';
+        } else {
+            rowOne = rowOne + ' ';
+            rowTwo = rowTwo + '#';
+        }
+    
+    }
+
+    for(i=0; i<size; i++){
+        //console.log(i);
+        if(i%2 == 0){
+            console.log(rowOne);
+        } else {
+            console.log(rowTwo);
+        }
+    }
+}
+triangle();
+fizzBuzz();
+chessBoard(size);
