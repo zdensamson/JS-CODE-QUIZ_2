c1a3e4026fbed61bf25e3e661d281b07e47cbc34
Deku <zachdsamson@gmail.com>
2023-03-01 20:36:44 -0700

    game cycles through questions and posts score with a player highScore form that leads to nothing

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,74 +1,15 @@
-// var number = 5;
-// var triangle= function() {
-//     let hash = ''
-//     for (i=0; i<7; i++) {
-//         hash = hash + '#'
-//         console.log(hash);
-//     }
-// };
-// var fizzBuzz = function() {
-//     for (i=1; i<=100 ; i++) {
-//         if (i%3 == 0 && i%5 ==0){
-//             console.log('fizzbuzz')
-//         } else if(i%3==0){
-//             console.log('fizz');
-//         } else if (i%5 == 0) {
-//             console.log('buzz');
-//         } else {
-//             console.log(i);
-//         }
+// case for timer hitting 0 
+// localStorage of scores
+// ranking scores
+// restarting game 
 
-//     }
-// }
-// let size = 10;
-// var chessBoard = function(size) {
-//     let rowOne = ' ';
-//     let rowTwo = '#';
-//     for(i=0; i<size; i++){
-//         if(i%2 == 0) {
-//             rowOne = rowOne + '#';
-//             rowTwo = rowTwo + ' ';
-//         } else {
-//             rowOne = rowOne + ' ';
-//             rowTwo = rowTwo + '#';
-//         }
-//     }
-//     for(i=0; i<size; i++){
-//         //console.log(i);
-//         if(i%2 == 0){
-//             console.log(rowOne);
-//         } else {
-//             console.log(rowTwo);
-//         }
-//     }
-// }
-// triangle();
-// fizzBuzz();
-// chessBoard(size);
-// var findMin = function(a,b) {
-//     if(a < b) {
-//         console.log(a)
-//     } else {
-//         console.log(b)
-//     }
-// };
-// var isEven = function(number) {
-//     if(number < 0){
-//         isEven(-1*number)
-//     }
-//     else if(number == 1){
-//         console.log("ODD")
-//     } else if(number == 0){
-//         console.log("EVEN")
-//     } else {
-//         isEven(number - 2)
-//     }
-// }
 
+var gameEl = document.querySelector("#game-shell");
 var startBtnEl = document.querySelector(".start");
 var timeBoxEl = document.querySelector(".timer");
 var questionBoxEl = document.querySelector(".question");
 var optionBoxEl = document.querySelector("#answer-container");
+var responseBoxEl = document.querySelector(".response");
 
 var exampleBoxEl = document.querySelector("#example");
 
@@ -76,69 +17,85 @@ var answerObj = {
     "The Hulk": ["Spider Man", "Iron Man", "Super Man", "The Hulk"],
     "Spider Man": ["Nacho Libre", "Spider Man", "Mr. Fantastic", "Star Lord"],
     "The Batman": ["Ant Man", "Green Lantern", "The Batman", "Plastic Man"]
-}
-var questionList = ["Who is Bruce Banner", "Who is Peter Parker?","Who is Bruce Wayne"]
+};
 
-// console.log(Object.keys(answerObj)[0]);
-// console.log(Object.values(answerObj)[0]);
-// console.log(Object.entries(answerObj)[0]);
+var questionList = ["Who is Bruce Banner", "Who is Peter Parker?","Who is Bruce Wayne"];
+var questionNumber = 0;
+var timeSecond = 100;
+var gameActive = 0;
+var errorCount = 0;
 
-
-var questionQueue = function() {
-    questionBuilder(questionList[0]);
-    optionBuilder(Object.keys(answerObj)[0],Object.values(answerObj)[0]);
-}
+var questionQueue = function(qNumber) {
+    questionBuilder(questionList[qNumber]);
+    optionBuilder(Object.keys(answerObj)[qNumber],Object.values(answerObj)[qNumber]);
+};
 
 var questionBuilder = function(question) {
     questionBoxEl.textContent = question;
-}
+};
 
 var optionBuilder = function(answer, options) {
-    var currentAnswer = answer; 
-    console.log(options);
-
+    optionBoxEl.innerHTML = ''
     for(var i = 0; i < options.length; i++){
         var optionEl = document.createElement("div");
         optionEl.className = "box";
         if(options[i] == answer) {
             optionEl.setAttribute("data-boolean", "correct");
         } 
-        // else {
-        //     optionEl.setAttribute("data-boolean", "incorrect");
-        // }
-        // optionEl.className.textContent = options[i];
         optionEl.textContent = options[i];
         optionBoxEl.append(optionEl);
     }
 
-}
+};
 
 var selectedAnswerHandler = function(event) {
-    console.log(event.target);
+
     var targetEl = event.target;
-    console.log(questionBoxEl.textContent);
+
     if (targetEl.getAttribute("data-boolean") == "correct") {
-        console.log("CORRECT");
-        // fucntion to load next question
+        responseBoxEl.textContent = "CORRECT";
+        questionNumber ++;
+        if(questionNumber == questionList.length) {
+            gameWin();
+        } else {
+            questionQueue(questionNumber);
+        }
     }
     else {
-        console.log("INCORRECT");
+        responseBoxEl.textContent = "INCORRECT";
+        timeSecond = timeSecond -10;
+        errorCount ++;
     }
-}
+};
+
+var gameWin = function(){
+    gameEl.innerHTML = "";
+    gameEl.textContent = `You got a score of ${timeSecond} seconds, and guessed incorrectly ${errorCount} time(s) `
+    var scoreForm = document.createElement("form");
+    scoreForm.setAttribute("action", "");
+    var scoreInput = document.createElement("input");
+    var scoreSubmit = document.createElement("button");
+    scoreSubmit.setAttribute("type", "submit");
+    scoreSubmit.textContent = "Submit score";
+
+    scoreForm.appendChild(scoreInput);
+    scoreForm.appendChild(scoreSubmit);
+    gameEl.appendChild(scoreForm);
+};
 
 var startQuiz = function(){
-
+    
+    gameActive = 1;
     var countDown = function(){
-        var timeSecond = 100;
+       
         const countDown = setInterval(() => {
             timeSecond--;
             timeBoxEl.innerHTML = timeSecond;
         }, 1000);
     }
-    questionQueue();
-    // questionBoxEl.textContent = questionList[0];
+    questionQueue(questionNumber);
     countDown();
-}
+};
 
 
 startBtnEl.addEventListener("click",startQuiz);
